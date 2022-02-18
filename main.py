3282ab01f91bf744a673eb3741443996f0ca0286
SERGIO <jsergiobarros@gmail.com>
2022-02-18 00:11:15 -0300

    tk ok, funcoes meh

--- main.py
+++ main.py
@@ -11,14 +11,42 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host,port))
 sock.listen()
 conn, end = sock.accept()'''
+Vazia=2
+
+
+
+def move(x):
+    global Vazia
+    bot[Vazia]["image"]=piece[1]
+    bot[0]["image"] = piece[0]
+    Vazia=0
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-tabuleiro = [0, 0, 0, 0, 0, 0, 0]
+tabuleiro = [0, 0,0, 0, 0, 0, 0]
+adv=1
+j=0
+def preenche(x):
+    global adv,j,Vazia
+    print(x,adv,j, tabuleiro)
 
+    if tabuleiro[x] ==0:
+        tabuleiro[x]=adv
+        bot[x]["image"]=piece[adv]
+        j=j+1
+        vencedor()
+        if j==6:
+           Vazia = tabuleiro.index(0)
+        if adv==1:
+            adv=2
+        elif adv==2:
+            adv=1
+    else:
+        return 0
 
 def vencedor():
     if tabuleiro[0] == tabuleiro[1] == tabuleiro[4] and tabuleiro[0] != 0:
@@ -37,23 +65,35 @@ def prt(x):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    tabuleiro[2] = '1.1.1'
-    tabuleiro[1] = '1.1.1'
-    tabuleiro[3] = '1.1.1'
     print(tabuleiro)
     vencedor()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
 janela = Tk()
+piece= [PhotoImage(file="bola0.png"),PhotoImage(file="bola1.png"),PhotoImage(file="bola2.png")]
 janela.geometry("700x350")
 janela.title("Tsoro Yematatu")
-bot=[0,0,0,0,0,0,0]
-bot[0]= Button(janela,bg="red",width=3,borderwidth=0)
-bot[1]= Button(janela,bg="red",width=3,borderwidth=0)
-bot[2]= Button(janela,bg="red",width=3,borderwidth=0)
-bot[3]= Button(janela,bg="red",width=3,borderwidth=0)
-bot[4]= Button(janela,bg="red",width=3,borderwidth=0)
-bot[5]= Button(janela,bg="red",width=3,borderwidth=0)
-bot[6]= Button(janela,bg="red",width=3,borderwidth=0)
+canvas=Canvas(janela, width=700, height=350)
+canvas.create_line(210,20,370,200,fill="black",width=2)
+canvas.create_line(220,20,60,200,fill="black",width=2)
+canvas.create_line(217,20,217,200,fill="black",width=2)
+canvas.create_line(130,105,270,105,fill="black",width=2)
+canvas.create_line(60,185,340,185,fill="black",width=2)
+canvas.pack(pady=10)
+bot=[0,0,1,0,2,0,0]
+func=[0,0,0,0,0,0,0]
+bot[0] = Button(janela, image=piece[tabuleiro[0]], borderwidth=0,command=lambda:preenche(0))
+
+bot[1] = Button(janela, image=piece[tabuleiro[1]], borderwidth=0,command=lambda:preenche(1))
+bot[2] = Button(janela, image=piece[tabuleiro[2]], borderwidth=0,command=lambda:preenche(2))
+bot[3] = Button(janela, image=piece[tabuleiro[3]], borderwidth=0,command=lambda:preenche(3))
+
+bot[4] = Button(janela, image=piece[tabuleiro[4]], borderwidth=0,command=lambda:preenche(4))
+bot[5] = Button(janela, image=piece[tabuleiro[5]], borderwidth=0,command=lambda:preenche(5))
+bot[6] = Button(janela, image=piece[tabuleiro[6]], borderwidth=0,command=lambda:preenche(6))
+adv=1
+#while i <6:
+
 
 bot[0].place(x=200,y=20)
 
@@ -61,12 +101,11 @@ bot[1].place(x=130,y=100)
 bot[2].place(x=200,y=100)
 bot[3].place(x=270,y=100)
 
-bot[4].place(x=60,y=200)
-bot[5].place(x=200,y=200)
-bot[6].place(x=340,y=200)
+bot[4].place(x=60,y=180)
+bot[5].place(x=200,y=180)
+bot[6].place(x=340,y=180)
 chat=Text(janela,width=700,height=10)
 chat.place(x=0,y=320)
-canvas=Canvas(janela, width=500, height=100)
-canvas.create_line(100,200,200,35, fill="green", width=5)
-canvas.place()
+
+
 janela.mainloop()
