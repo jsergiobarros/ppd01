c115c0fe3cb600e60f58009d5e5605b6522744c4
SERGIO <jsergiobarros@gmail.com>
2022-02-21 00:03:08 -0300

    tst pra comunicacao

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # This is a sample Python script.
+import tkinter
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk,messagebox
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import socket
 
+janela = Tk()
+janela.resizable(False,False)
 '''
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,30 +17,52 @@ def main():
         client.listen(1)
         conn,addr=client.accept()
         print(conn, addr)
+        
     except:#segundo a conectar sera cliente
         client.connect(('localhost', 50000))
         name = input("nome")
         client.send()
         print('x')
-
+    thread1 = 
+    thread2 =
+    thread1.start()  
+    thread2.start
 def recieveMsg(client):
-    pass #thread de envio de dados
+     #thread de receber de dados
+    while True:
+        jsonReceived = client.recv(1024)
+        print "Json received -->", jsonReceived
 def sendMsg(client):
-    pass #thread de recebimento de dados
+    while True:
+        x=input("digita\n") #thread de envio de dados
+        jsonResult = {"first":"You're", "second":x}
+        jsonResult = json.dumps(jsonResult)
+        client.send(jsonResult) 
 '''
 Vazia = 2
 
 
 def move(x):
     global Vazia
-    bot[Vazia]["image"] = piece[1]
-    bot[0]["image"] = piece[0]
-    Vazia = 0
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    if x ==1 and (Vazia==5 or Vazia== 6):
+        pass
+    elif x ==2 and (Vazia==4 or Vazia== 6):
+        pass
+    elif x ==3 and (Vazia==5 or Vazia== 4):
+        pass
+    elif x ==4 and (Vazia==2 or Vazia== 3):
+        pass
+    elif x ==5 and (Vazia==1 or Vazia== 3):
+        pass
+    elif x ==6 and (Vazia==1 or Vazia== 2):
+        pass
+    else:
+        bot[Vazia]["image"] = piece[tabuleiro[x]]
+        bot[x]["image"] = piece[0]
+        tabuleiro[x],tabuleiro[Vazia]=0,tabuleiro[x]
+        Vazia = x
+        vencedor()
+        print(tabuleiro)
 
 
 tabuleiro = [0, 0, 0, 0, 0, 0, 0]
@@ -47,7 +72,6 @@ j = 0
 
 def preenche(x):
     global adv, j, Vazia
-    print(x, adv, j, tabuleiro)
     if j < 6:
         if tabuleiro[x] == 0:
             tabuleiro[x] = adv
@@ -63,6 +87,7 @@ def preenche(x):
         else:
             return 0
     else:
+        move(x)
         print("x")  # jogo comeca
 
 
@@ -79,21 +104,28 @@ def vencedor():
         print("win")
 
 
-def prt(x):
-    print(x)
+def lbds():
+    bot[0]["command"] = lambda: preenche(0)
+    bot[1]["command"] = lambda: preenche(1)
+    bot[2]["command"] = lambda: preenche(2)
+    bot[3]["command"] = lambda: preenche(3)
+    bot[4]["command"] = lambda: preenche(4)
+    bot[5]["command"] = lambda: preenche(5)
+    bot[6]["command"] = lambda: preenche(6)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     print(tabuleiro)
     vencedor()
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-janela = Tk()
+
 piece = [PhotoImage(file="bola0.png"), PhotoImage(file="bola1.png"), PhotoImage(file="bola2.png")]
 janela.geometry("700x350")
 janela.title("Tsoro Yematatu")
+
 canvas = Canvas(janela, width=700, height=350)
 canvas.create_line(210, 20, 370, 200, fill="black", width=2)
 canvas.create_line(220, 20, 60, 200, fill="black", width=2)
@@ -101,8 +133,10 @@ canvas.create_line(217, 20, 217, 200, fill="black", width=2)
 canvas.create_line(130, 105, 270, 105, fill="black", width=2)
 canvas.create_line(60, 185, 340, 185, fill="black", width=2)
 canvas.pack(pady=10)
-bot = [0, 0, 1, 0, 2, 0, 0]
-func = [0, 0, 0, 0, 0, 0, 0]
+bot = [0, 0, 0, 0, 0, 0, 0]
+def popUp():
+    messagebox.askquestion(title="inicie o jogo ", message="inicie o jogo primeiro")
+
 bot[0] = Button(janela, image=piece[tabuleiro[0]], borderwidth=0, command=lambda: preenche(0))
 
 bot[1] = Button(janela, image=piece[tabuleiro[1]], borderwidth=0, command=lambda: preenche(1))
@@ -111,8 +145,10 @@ bot[3] = Button(janela, image=piece[tabuleiro[3]], borderwidth=0, command=lambda
 
 bot[4] = Button(janela, image=piece[tabuleiro[4]], borderwidth=0, command=lambda: preenche(4))
 bot[5] = Button(janela, image=piece[tabuleiro[5]], borderwidth=0, command=lambda: preenche(5))
-bot[6] = Button(janela, image=piece[tabuleiro[6]], borderwidth=0, command=lambda: preenche(6))
+bot[6] = Button(janela, image=piece[tabuleiro[6]], borderwidth=0,command=popUp)
 adv = 1
+bott=Button(janela, image=piece[0], borderwidth=0, command=lbds)
+bott.place(x=60, y=250)
 
 bot[0].place(x=200, y=20)
 
@@ -123,7 +159,14 @@ bot[3].place(x=270, y=100)
 bot[4].place(x=60, y=180)
 bot[5].place(x=200, y=180)
 bot[6].place(x=340, y=180)
-chat = Text(janela, width=700, height=10)
-chat.place(x=0, y=320)
+chat = Text(janela, width= 35, height=17, state=DISABLED)
+chat.place(x=400, y=10)
+caixa = Text(janela, width= 35, height=1)
+caixa.place(x=400, y=300)
+
+
+inputValue=caixa.get("1.0","end-1c")
+print(inputValue)
+chat.insert(END,inputValue)
 
 janela.mainloop()
---
+++ tst.py
@@ -0,0 +1,51 @@
+import json
+import socket
+import threading
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:  # primeiro que conectar sera servidor
+        client.bind(('localhost', 50000))
+        print("escutando")
+        client.listen(1)
+        conn, addr = client.accept()
+        print(conn, addr)
+        thread1 = threading.Thread(target=recieveMsg, args=[conn])
+        print("t1")
+        thread2 = threading.Thread(target=sendMsg, args=[conn])
+        print("t2")
+        thread1.start()
+        print("t1")
+        thread2.start()
+        print("t2")
+    except:  # segundo a conectar sera cliente
+        client.connect(('localhost', 50000))
+        name = input("nome\n")
+
+        client.send(name.encode('utf-8'))
+        print('x')
+
+
+
+
+def recieveMsg(conn):
+    # thread de receber de dados
+    while True:
+        print(conn)
+
+        jsonReceived = conn.recv(1024)
+        if jsonReceived:
+            print("Json received -->", jsonReceived.decode('utf-8'))
+
+def sendMsg(conn):
+    while True:
+
+        x = input("digita\n")  # thread de envio de dados
+        jsonResult = {"first": "You're", "second": x}
+        print(jsonResult)
+        jsonResult = json.dumps(jsonResult)
+        conn.send(x.encode())
+
+main()
